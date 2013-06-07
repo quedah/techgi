7968e2e5a8e12e04f3ec0c1324d53d27412f5945
Ed <ed@quedah.com>
2013-06-07 21:08:26 +0200

    Fixed vectorclock in client.py, removed fastclient

--- PR3/client.py
+++ PR3/client.py
@@ -24,13 +24,41 @@ class VectorClock(dict):
             for node in nodes:
                 name = node.getElementsByTagName('Name')[0].firstChild.nodeValue
                 t = node.getElementsByTagName('Time')[0].firstChild.nodeValue
-                self[name] = t
+                self[name] = int(t)
+
+    def __lt__(self, other):
+        """
+        Check if all entries in self are <= and at least one entry strictly <
+        than in other.
+        """
+        other_names = other.keys()
+        own_names = self.keys()
+        diff_count = 0      # Count of elements strictly smaller in self.
+        for name in own_names:
+            if name not in other_names:
+                return False
+            elif self[name] > other[name]:
+                return False
+            elif self[name] < other[name]:
+                diff_count += 1
+
+        if diff_count > 0:
+            return True
+        else:
+            return False
+
+    def __gt__(self,other):
+        """
+        Check if all entries in self are >= and at least one entry strictly >
+        than in other.
+        """
+        return other < self
 
     def increment(self):
         """
         Increment own time by 1.
         """
-        self[self.name] += 1
+        self[self.name] += int(1)
 
     def toxml(self, xmldoc ):
         root = xmldoc.createElement('VC')
@@ -52,24 +80,13 @@ class VectorClock(dict):
 
     def sync(self, other):
         """
-        Checks if this and other are properly synced. If there are missing
-        messages returns False and only entry for 'other' is incremented. If
-        there are no missing messages returns True and everything is
-        incremented.
+        Set own clock to max of other clock.
         """
-        self[other.name] = other[other.name]
         other_names = other.keys()
-        self_names = self.keys()
-        if self.name in other_names:    other_names.remove(self.name)
-        other_names.remove(other.name)
-
+        own_names = self.keys()
         for name in other_names:
-            if name not in self_names:
-                self[name] = other[name]        # Add prev. unknown name.
-            elif self[name] < other[name]:
-                return False                    # Missed s.th., stash for now.
-
-        return True                             # Everything OK.
+            if name not in own_names:  self[name] = int(other[name])
+            elif self[name] < other[name]:  self[name] = int(other[name])
 
 class Client(Mosquitto,object):
     def __init__(self,host, port, name=None):
@@ -84,9 +101,10 @@ class Client(Mosquitto,object):
         self.on_message = self.unpack
 
         self.connect(self.host, self.port, 60)
-        self.subscribe(SUBS,1)
+        self.subscribe(SUBS,0)
 
-        self.vc = VectorClock(self.name)
+        self.vc = VectorClock(self.name)            # VC for sending data.
+        self.print_vc = VectorClock(self.name)      # VC of last printed msg.
         self.stash = [] # Store out-of-sync messages here until printed.
 
     def run(self):
@@ -96,8 +114,11 @@ class Client(Mosquitto,object):
         """
         q = Queue.Queue()
         t = threading.Thread(target=self.receive)
+        t2 = threading.Thread(target=self.check_stash)
         t.daemon = True
+        t2.daemon = True
         t.start()
+        t2.start()
 
         self.talk()
 
@@ -124,8 +145,9 @@ class Client(Mosquitto,object):
                     time.sleep(1)
                     sys.exit()
                 self.vc.increment()
+                self.print_vc.increment()
                 txt = self.pack(txt)
-                self.publish("Gossip",txt,1)
+                self.publish("Gossip",txt,0)
 
     def pack(self, msg):
         dom = getDOMImplementation()
@@ -166,36 +188,67 @@ class Client(Mosquitto,object):
 
         # Get Vector Clock
         vc_dom = dom.getElementsByTagName('VC')
-        if len(vc_dom) > 0:
-            # Received legitimate message.
-            mqtt.vc.increment()
-            other_vc = VectorClock(name,xmlnode=vc_dom[0])
-            in_sync = mqtt.vc.sync(other_vc)
-            n = len(mqtt.stash) - 1
-            for i, stashed_msg in enumerate(reversed(mqtt.stash)):
-                if mqtt.vc.sync(stashed_msg[0]):
-                    mqtt.print_msg(stashed_msg[1])
-                    mqtt.stash.pop(n - i)   # Remove msg from stash.
-        else:
-            return None         # Abort, invalid data.
-
-        # Get Message
         message = dom.getElementsByTagName('Message')
-        if len(message) > 0:
-            message = message[0]
-            payload = dom.getElementsByTagName('Payload')[0].firstChild.nodeValue
-            if in_sync:     mqtt.print_msg(name, payload)
-            else:           stash.append((other_vc, payload)) 
+        if len(vc_dom) == 0:    return None     # No attached VC. 
+        if len(message) == 0:   return None     # No attached message.
 
+        # Retrieve data.
+        other_vc = VectorClock(name,xmlnode=vc_dom[0])
+        payload = dom.getElementsByTagName('Payload')[0].firstChild.nodeValue
+        message = message[0]
 
-    def print_msg(self, name, payload):
+        mqtt.vc.sync(other_vc)
+
+        if mqtt.printable(other_vc):    
+            mqtt.print_msg(other_vc, payload)
+        else:           
+            mqtt.stash.append((other_vc, payload)) 
+
+    def check_stash(self):
+        while True:
+            time.sleep(1)
+            n = len(self.stash) - 1
+            for i, (other_vc, msg) in enumerate(reversed(self.stash)):
+                if self.printable(other_vc):
+                    self.print_msg(other_vc, msg)
+                    self.stash.pop(n - i)   # Remove msg from stash.
+
+    def print_msg(self, other_vc, payload):
+        name = other_vc.name
         print "\r\r\r\r\r\r\r\r"+RED+name+10*" "+"\n<---"+DEFAULT,
         print "    {0}".format(payload)
         print "\n"+GREEN+"--->    "+DEFAULT,
         sys.stdout.flush()
+        self.print_vc.sync(other_vc)
+
+    def printable(self, other_vc):
+        """
+        Checks if this message can be printed as is. Returns False if message
+        has to be stashed, i.e. there is some other unprinted message that comes
+        first. Else returns True.
+        """
+        #return True
+        other_names = other_vc.keys()
+        own_names = self.print_vc.keys()
+
+        for name in other_names:
+            if name in own_names:
+                # Sender can be incremented by up to +1.
+                if name == other_vc.name:
+                    if other_vc[name] > self.print_vc[name] + 1:
+                        #print "SENDER: ", name, ": ", other_vc
+                        #print "SELF: ", self.print_vc
+                        return False
+                # Everyone else must have values smaller or equal.
+                elif other_vc[name] > self.print_vc[name]:
+                    #print "SUBJECT: ", name, ": ", other_vc
+                    #print "SELF: ", self.print_vc
+                    return False
+
+        return True
+
 
 def main():
-    vc = VectorClock("Ed")
     args = sys.argv
     if len(args) < 3:   sys.exit("usage: ./client host port [id]")
     else:               
