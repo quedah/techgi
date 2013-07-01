21131c51507716cc25313da47d80820254099d38
e.din <e.din@cs.tu-berlin.de>
2013-07-01 13:02:47 +0200

    Got Sender and Receiver to compile -> see Ed's version.

--- techGI4_PR2_Server/src/de/tuberlin/techgi4/gpscalcWebservice/WSPublisher.java
+++
@@ -1,9 +0,0 @@
-package de.tuberlin.techgi4.gpscalcWebservice;
-
-import javax.xml.ws.Endpoint;
-
-public class WSPublisher {
-	public static void main(String[] args) {
-		Endpoint.publish("http://localhost:8080/WS/Greeting",new GPSCalcImpl());
-	}
-}
